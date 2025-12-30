e1aecb318278a67f9c97a36d66c13f791338ded9
amir bagherzadeh dizaji <dizaji74@gmail.com>
2025-12-30 22:48:12 +0330

    Project connected to DB.

---
+++ utils/connectDB.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+async function connectDB() {
+  if (mongoose.connections[0].readyState) return;
+  await mongoose.connect("mongodb+srv://dizaji74:B123456789b@cluster0.7ekhiag.mongodb.net/");
+  console.log("Connected to DB");
+}
+
+export default connectDB;
