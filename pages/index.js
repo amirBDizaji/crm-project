276bafa7c8991ed15c489d16e604a86f88c8d35d
amir bagherzadeh dizaji <dizaji74@gmail.com>
2026-01-08 17:09:22 +0330

    The home page was created as SSR and connected to the database.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <h1>CRM Project</h1>;
+import HomePage from "@/components/template/HomePage";
+import Customer from "@/models/Customer";
+import connectDB from "@/utils/connectDB";
+import { notFound } from "next/navigation";
+
+function Index({ customers }) {
+  console.log(customers);
+  return <HomePage customers={customers} />;
+}
+
+export default Index;
+
+export async function getServerSideProps() {
+  try {
+    await connectDB();
+    const customers = await Customer.find();
+    return {
+      props: {
+        customers: JSON.parse(JSON.stringify(customers)),
+      },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
 }
